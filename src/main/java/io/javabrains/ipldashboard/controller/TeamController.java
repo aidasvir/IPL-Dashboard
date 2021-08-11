8964f9ce30df0555c377eb21f1e406a71c1b9bb8
Aidas <aidas.vir@gmail.com>
2021-08-11 19:27:13 +0300

    React app with Team page

--- src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -2,6 +2,7 @@ package io.javabrains.ipldashboard.controller;
 
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import io.javabrains.ipldashboard.repository.TeamRepository;
 
 
 @RestController
+@CrossOrigin
 public class TeamController {
         
     private TeamRepository teamRepository;
